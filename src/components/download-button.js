37ddf7e6290fb6d1b15db7cd8faed7b2b81cd2e5
nijikokun <nijikokun@gmail.com>
2020-04-29 00:01:19 -0700

    feat(index) add download link to hero image

--- src/components/download-button.js
+++ src/components/download-button.js
@@ -1,49 +1,11 @@
 import React from 'react';
 import classnames from 'classnames';
-import {links} from '../config';
-import Link from './link';
+import DownloadLink from './download-link';
 
-class DownloadButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      platform: '__UNSET__'
-    };
-  }
-
-  componentDidMount() {
-    const isMobile = window.innerWidth <= 800 && window.innerHeight <= 600;
-    this.setState({
-      platform: isMobile ? 'Mobile' : navigator.platform.toLowerCase(),
-    });
-  }
-
-  render() {
-    const {platform} = this.state;
-    const {className, children} = this.props;
-
-    let href = links.download;
-
-    let platformName = 'Desktop';
-    if (platform.indexOf('mac') !== -1) {
-      platformName = 'Mac';
-      href = '/download/#mac';
-    } else if (platform.indexOf('win') !== -1) {
-      platformName = 'Windows';
-      href = '/download/#windows';
-    } else if (platform.indexOf('linux') !== -1) {
-      platformName = 'Linux';
-      href = '/download/#linux';
-    }
-
-    const message = children || `Download for ${platformName}`;
-
-    return (
-      <Link to={href} className={classnames('button', className)}>
-        {message}
-      </Link>
-    );
-  }
+export default function DownloadButton(props) {
+  return (
+    <DownloadLink className={classnames('button', props.className)}>
+      {props.children}
+    </DownloadLink>
+  );
 }
-
-export default DownloadButton
