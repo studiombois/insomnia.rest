5c184532a53d9a0c34e71671d4ee21f810140c5d
Alex Ordonez <a-p-o@users.noreply.github.com>
2020-05-05 16:00:51 -0700

    Update brew cask install command (#42)

--- src/pages/download/designer.js
+++ src/pages/download/designer.js
@@ -55,7 +55,7 @@ export default class extends React.Component {
                   <p
                     className="subtle small"
                     style={{ maxWidth: '12rem', margin: 'auto' }}>
-                    or <code>brew cask install insomnia</code>
+                    or <code>brew cask install insomnia-designer</code>
                   </p>
                 </div>
                 <div className="col-4 platform-download padding-bottom">
