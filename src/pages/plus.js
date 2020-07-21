b50968daac9f4471b3331390836bd9dcef611a04
Nijiko Yonskai <nijikokun@gmail.com>
2020-07-21 12:05:44 -0700

    fix(pages) plus page header (#56)

--- src/pages/plus.js
+++ src/pages/plus.js
@@ -7,21 +7,28 @@ import Companies from '../partials/companies';
 export default () => (
   <React.Fragment>
     <Helmet>
-      <body data-template="skinny" data-navbar="floating"/>
+      <body data-template="skinny" data-navbar="floating" />
     </Helmet>
     <div className="jumbotron">
-      <div className="container container--skinny">
-        <div className="row">
-          <div className="col-12 center">
-            <h1>Insomnia Plus</h1>
-            <h2 className="font-light">
-              Your API workspaces, backed up and in sync
-            </h2>
-            <Link to="/pricing/" className="button">Start Free Trial</Link>
+      <div className="jumbotron-inner">
+        <div className="container slogan">
+          <div className="row">
+            <div className="col-12 center">
+              <h1 className="font-regular">Insomnia Plus</h1>
+              <h2 className="font-light">
+                Your API workspaces, backed up and in sync
+              </h2>
+              <p>
+                <Link to="/pricing/" className="button">
+                  Start Free Trial
+                </Link>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="jumbotron__img-container">
-          <img src={mainSrc} className="jumbotron__img" alt="Insomnia Plus"/>
+
+          <div className="container">
+            <img src={mainSrc} className="jumbotron__img" alt="Insomnia Plus" />
+          </div>
         </div>
       </div>
     </div>
@@ -31,34 +38,40 @@ export default () => (
           Sync your API workspaces across devices
         </h2>
         <p>
-          Create an account, log in, and relax. With Plus, your application data will be automatically
-          backed on the cloud and synced across all your devices.
-          <br/><br/>
+          Create an account, log in, and relax. With Plus, your application data
+          will be automatically backed on the cloud and synced across all your
+          devices.
+          <br />
+          <br />
         </p>
-        <p><strong>Automatic or manual sync</strong> 🕰 choose when to sync</p>
-        <p><strong>End-to-end encryption</strong> 🔐 know your data is safe</p>
-        <p><strong>Sync unlimited data</strong> 🚀 never worry about limits</p>
-        <hr/>
-        <h2 id="start-using-plus-for-free">
-          Start using Plus for free
-        </h2>
         <p>
-          Sign up to start a 14-day free trial (no
-          credit card required).
+          <strong>Automatic or manual sync</strong> 🕰 choose when to sync
         </p>
+        <p>
+          <strong>End-to-end encryption</strong> 🔐 know your data is safe
+        </p>
+        <p>
+          <strong>Sync unlimited data</strong> 🚀 never worry about limits
+        </p>
+        <hr />
+        <h2 id="start-using-plus-for-free">Start using Plus for free</h2>
+        <p>Sign up to start a 14-day free trial (no credit card required).</p>
         <p className="center">
-          <br/>
-          <Link className="button" to="/pricing/">Start Free Trial</Link>
+          <br />
+          <Link className="button" to="/pricing/">
+            Start Free Trial
+          </Link>
         </p>
       </div>
     </section>
     <section className="dark no-margin padding-top padding-bottom center">
       <h2>Over 300,000 developers trust Insomnia</h2>
-      <br/>
-      <Companies/>
-      <br/>
-      <Link to="/pricing/" className="button button--big">Get Started</Link>
+      <br />
+      <Companies />
+      <br />
+      <Link to="/pricing/" className="button button--big">
+        Get Started
+      </Link>
     </section>
   </React.Fragment>
 );
-
