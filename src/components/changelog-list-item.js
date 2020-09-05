f82df50168184e662a9828f2dd467144e8d16fd2
Opender Singh <opender94@gmail.com>
2020-09-05 16:56:05 +1200

    Split the code a little and make it easier to follow

--- src/components/changelog-list-item.js
+++ src/components/changelog-list-item.js
@@ -1,33 +1,47 @@
 import React from 'react';
 
-const PR_LINK_SEGMENTED_REGEX = /\(PR:(\d+)(:([^)]+))?\)/g;
-const PR_LINK_REGEX = /(\(PR:\d+:[^)]+?\))/;
+// There is no concept of typing in this project yet, so adding here for future reference.
 
-const ChangelogListItem = ({ text }) => {
-  const matches = text.match(PR_LINK_SEGMENTED_REGEX);
-  if (!matches?.length) {
-    return text;
-  }
-
+// Convert (PR:123:abc) to anchors
+// Returns type Replacements = { [string]: React.ReactNode };
+// If there are no tags present, this will simply return an empty object
+const findTagReplacements = (text) => {
+  const PR_LINK_SEGMENTED_REGEX = /\(PR:(\d+)(:([^)]+))?\)/g;
   const replacements = {};
+
   let match;
   while (match = PR_LINK_SEGMENTED_REGEX.exec(text)) {
     const prNumber = match[1];
     const user = match[3] || '';
     const userString = (user ? ' by ' + user : '');
     const replacement = (
-      <a href={`https://github.com/Kong/insomnia/pull/${prNumber}`} target="_blank">
+      <a key={`${prNumber}${userString}`} href={`https://github.com/Kong/insomnia/pull/${prNumber}`} target="_blank">
         (#{prNumber}{userString})
       </a>
     );
     replacements[match[0]] = replacement;
   }
 
+  return replacements;
+}
+
+// Split the input text and inject anchors in place of tags
+// Returns an array of React nodes.
+const generateNodes = (text, replacements) => {
+  const PR_LINK_REGEX = /(\(PR:\d+:[^)]+?\))/;
   const segments = text.split(PR_LINK_REGEX);
-  return segments.reduce((fragments, curr) => {
+  const nodes = segments.reduce((fragments, curr) => {
     if (curr) { fragments.push(replacements[curr] || curr); }
     return fragments;
   }, []);
+
+  return nodes;
+}
+
+const ChangelogListItem = ({ text }) => {
+  const replacements = React.useMemo(() => findTagReplacements(text), [text]);
+  const nodes = React.useMemo(() => generateNodes(text, replacements), [text, replacements]);
+  return nodes;
 };
 
-export default ChangelogListItem
+export default ChangelogListItem;
